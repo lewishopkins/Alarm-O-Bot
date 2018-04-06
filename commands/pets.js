b078a825d51e157a15ec5f3b19a88f8caeb988af
Lewis Hopkins <me@lewishopkins.co.uk>
2018-04-06 16:29:01 +0100

    Modified pets.js

--- commands/pets.js
+++ commands/pets.js
@@ -4,6 +4,7 @@
 exports.run = (client, message, args) => {
  
     const config = require("../config.json");
+    const validator = require("../functions/character-validator.js");
     const fs = require("fs");
     const blizzard = require('blizzard.js').initialize({ apikey: config.BLIZZARD_API_KEY });
 
@@ -16,14 +17,19 @@ exports.run = (client, message, args) => {
         // Valid Details - Set Details
         var Cname = characterDetails[0];
         var Crealm = characterDetails[1];
-        var Cregion = characterDetails[2]; 
+        var Cregion = characterDetails[2];
+
+        // Queries
+        var Ppets = blizzard.wow.character(['profile', 'pets'], { realm: Crealm, name: Cname, origin: Cregion });
+        var PpetSlots = blizzard.wow.character(['petSlots'], { realm: Crealm, name: Cname, origin: Cregion })
 
 
-        blizzard.wow.character(['profile', 'pets'], { realm: Crealm, name: Cname, origin: Cregion })
-            .then(response => {
+        // Gather data
+		var results = Promise.all([Ppets, PpetSlots]);
+		results.then(response => {
 
-            console.log(response.data);
-            console.log("Pets Collected by " + response.data.name + ": " + response.data.pets.numCollected + "/" + response.data.pets.numNotCollected);
+            console.log(response[1].data);
+            console.log("Pets Collected by " + response[0].data.name + ": " + response[0].data.pets.numCollected + "/" + response[0].data.pets.numNotCollected);
             
         }).catch(err => message.reply("I was unable to find the character '" + Cname + "', on the realm '" + Crealm + "', on the '" + Cregion + "' region."));
     }
