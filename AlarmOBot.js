651fdd12ab5ab208c20bc3abe251f45ec18a4c94
Lewis Hopkins <me@lewishopkins.co.uk>
2019-02-14 17:42:20 +0000

    Promise rejection handling created for startup

--- AlarmOBot.js
+++ AlarmOBot.js
@@ -18,14 +18,21 @@ bot.on("ready", () => {
 		key: config.BLIZZARD_API_KEY,
 		secret: config.BLIZZARD_API_SECRET,
 		origin: config.default_region
-	}).then(response => {
-		console.log(response.data);
+	})
+	.then(response => {
+		// Write API Access Token
 		config.BLIZZARD_API_ACCESS_TOKEN = response.data.access_token;
 		fs.writeFile("./config.json", JSON.stringify(config), (err) => console.error);
-	});
 
-	console.log("\x1b[33m", "Ready to accept commands on Discord!");
-	bot.user.setGame(config.currently_playing);
+		console.log("\x1b[33m", "Ready to accept commands on Discord!");
+		bot.user.setGame(config.currently_playing);
+	})
+	.catch (err => {
+		// Promise Error/Exception Handling
+		console.log("\x1b[31m", "[ERROR] Issue validating your Battle.net credentials:");
+		console.log("\x1b[31m", "[ERROR] Error Code: " + err.response.status + " " + err.response.statusText);
+		return err;
+	});
 
 });
 
@@ -52,9 +59,12 @@ bot.on("message", message => {
 		let commandFile = require(`./commands/${command}.js`);
 		commandFile.run(bot, message, args, blizzard, config);
 	} catch (err) {
-		console.error(err);
+		if (config.DEBUG_commands)
+			console.error(err);
+
+		// Send error data to console
+		console.log("\x1b[31m", `[ERROR] Command ./commands/${command}.js attempted by user ${message.author.username}#${message.author.discriminator} [${message.author.id}] not found.`);
 	}
-	
 });
 
 // Discord Token
